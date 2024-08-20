d09ce60783ac722efb1485950c649edb46755c12
AlionaVr <alonavershilo@yandex.by>
2024-08-20 15:31:27 +0300

    new Class, which can rotate a matrix

---
+++ src/main/java/org/rotation/RotationOfTable.java
@@ -0,0 +1,63 @@
+package org.rotation;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RotationOfTable {
+    public static final int SIZE = 8;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] colors = new int[SIZE][SIZE];
+        Random random = new Random();
+        System.out.println("Начальная матрица");
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                colors[i][j] = random.nextInt(256);
+                System.out.format("%4d", colors[i][j]);
+            }
+            System.out.println();
+        }
+        while (true) {
+            System.out.println("Для завершения введите 'end'. Введите угол вращения матрицы (90,180 или 360): ");
+            String input = scanner.nextLine();
+            if (input.contains("end")) {
+                break;
+            } else {
+                int rotationAngle = Integer.parseInt(input);
+                if (rotationAngle == 90 || rotationAngle == 180 || rotationAngle == 360) {
+                    rotate(colors, rotationAngle);
+                } else System.out.println("Проверьте введенное значение");
+            }
+        }
+    }
+
+
+    public static void rotate(int[][] colors, int rotationAngle) {
+        int[][] rotatedColors = new int[SIZE][SIZE];
+        System.out.printf("Матрица повернута на %s градусов\n", (rotationAngle == 90) ? "90" : (rotationAngle == 180) ? "180" : "360");
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (rotationAngle == 90) {
+                    rotatedColors[i][j] = (colors[SIZE - 1 - j][i]);
+                } else if (rotationAngle == 180) {
+                    rotatedColors[i][j] = (colors[SIZE - 1 - i][SIZE - 1 - j]);
+                } else rotatedColors[i][j] = (colors[i][j]);
+
+
+                //int x =(rotationAngle == 90)?(SIZE - 1 - j):
+                //        (rotationAngle == 180)?(SIZE - 1 - i):i;
+                //int y = (rotationAngle == 90)?i:
+                //        (rotationAngle == 180)?(SIZE - 1 - j):j;
+                //rotatedColors[i][j]=colors[x][y];
+                //System.out.format("%4d", rotatedColors[i][j]);
+
+
+                System.out.format("%4d", rotatedColors[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
