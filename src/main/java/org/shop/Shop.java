4a78aaab87b737bcce0c7006ccf70bd49475453f
AlionaVr <alonavershilo@yandex.by>
2024-08-28 13:11:30 +0300

    Class structure was changed. Added separate methods: communicateWithCustomer,calculateAndShowBill, ShowMenu.

--- src/main/java/org/shop/Shop.java
+++ src/main/java/org/shop/Shop.java
@@ -6,15 +6,31 @@ import java.util.Scanner;
 
 public class Shop {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("F:/Projects/Netology/src/main/java/org/Shop/Order"));
-        System.out.println("Здравствуйте! Добро пожаловать в наш магазин. К сожалению, сегодня список продуктов ограничен\n" +
-                "1. Молоко 50 руб/шт\n" +
-                "2. Хлеб 14 руб/шт\n" +
-                "3. Гречневая крупа 80 руб/шт");
         String[] products = {"Молоко", "Хлеб", "Гречневая крупа"};
         int[] amounts = new int[products.length];
         int[] prices = {50, 14, 80};
+        showMenu(products, prices);
+        communicateWithCustomer(amounts);
+        calculateAndShowBill(products, amounts, prices);
+    }
+
+
+    private static void calculateAndShowBill(String[] products, int[] amounts, int[] prices) {
         double bill = 0;
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < amounts.length; i++) {
+            if (amounts[i] != 0) {
+                double priceForItems = amounts[i] * prices[i];
+                bill += priceForItems;
+                System.out.printf("%s %d шт %d руб/шт - %s руб в сумме \n", products[i], amounts[i], prices[i], priceForItems);
+            }
+        }
+        System.out.println(bill);
+    }
+
+
+    private static void communicateWithCustomer(int[] amounts) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("F:/Projects/Netology/src/main/java/org/Shop/Order"));
         while (true) {
             System.out.println("Выберите номер товар и введите количество или введите `end`\n");
             String input = scanner.nextLine();
@@ -28,17 +44,15 @@ public class Shop {
             }
             int productNumber = Integer.parseInt(parts[0]) - 1;
             int productCount = 0;
-            amounts[productNumber] = productCount + Integer.parseInt(parts[1]);
+            amounts[productNumber] += productCount + Integer.parseInt(parts[1]);
         }
-        System.out.println("Ваша корзина:");
-        for (int i = 0; i < amounts.length; i++) {
-            if (amounts[i] != 0) {
-                double priceForItems = amounts[i] * prices[i];
-                bill += priceForItems;
-                System.out.printf("%s %d шт %d руб/шт - %s руб в сумме \n", products[i], amounts[i], prices[i], priceForItems);
-            }
+    }
+
+    private static void showMenu(String[] products, int[] prices) {
+        System.out.println("Здравствуйте! Добро пожаловать в наш магазин. К сожалению, сегодня список продуктов ограничен\n");
+        for (int i = 0; i < products.length; i++) {
+            System.out.printf("%d. %s %d руб/шт\n", i + 1, products[i], prices[i]);
         }
-        System.out.println(bill);
     }
 }
 
