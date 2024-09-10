74e6080a0a63cc48c17a2366269bccf7740c2f10
AlionaVr <alonavershilo@yandex.by>
2024-09-10 16:01:17 +0300

    This is training with abstract methods and classes. CreditAccount and SimpleAccount extends abstract class Account.

---
+++ src/main/java/org/abstractions/Account.java
@@ -0,0 +1,33 @@
+package org.abstractions;
+
+public abstract class Account {
+    long balance;
+
+    public Account(long balance) {
+        this.balance = balance;
+    }
+
+    void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    long getBalance() {
+        return balance;
+    }
+
+    abstract boolean add(long amount);
+
+    abstract boolean pay(long amount);
+
+    boolean transfer(Account account, long amount) {
+
+        if (pay(amount)) {
+            if (account.add(amount)) {
+                return true;
+            } else {
+                add(amount);
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/org/abstractions/Main.java
@@ -0,0 +1,23 @@
+package org.abstractions;
+
+public class Main {
+    public static void main(String[] args) {
+        Account myAccount = new SimpleAccount(1800);
+        Account creditAccount = new CreditAccount(0, -1000);
+        Account HusbandAccount = new SimpleAccount(3200);
+
+        myAccount.add(200);
+        HusbandAccount.pay(200);
+        myAccount.transfer(HusbandAccount, 100);
+        System.out.println("HusbandAccount " + HusbandAccount.getBalance()); //3100
+
+        creditAccount.pay(800); //-800
+
+        myAccount.transfer(creditAccount, 1500);
+        System.out.println("creditAccount " + creditAccount.getBalance());//-800
+
+
+        System.out.println("myAccount " + myAccount.getBalance());//1900
+
+    }
+}
