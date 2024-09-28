5dc6b2955df5b95fa60028db2d4d10b7b460788a
AlionaVr <alonavershilo@yandex.by>
2024-09-28 15:24:39 +0300

    It's a practice of queue.

---
+++ src/main/java/org/appointmentForAManicure/Main.java
@@ -0,0 +1,24 @@
+package org.appointmentForAManicure;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> firstClients = List.of("Anya", "Sveta", "Olya", "Alexandra", "Ruslana", "Olesya", "Vika");
+        Queue<String> queue = new LinkedList<>(firstClients);
+
+        while (!queue.isEmpty()) {
+            String client = queue.poll();
+            System.out.println(client + " сделал новый маникюр");
+            if (Math.random() < 0.5 && !queue.isEmpty()) { // проверка условия, которое срабатывает с 50% вероятностью
+                queue.offer("a friend of " + client);
+
+            } else {
+                System.out.println("друг не записался");
+            }
+            System.out.println(queue);
+        }
+    }
+}
