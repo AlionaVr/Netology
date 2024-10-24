f0f2589ef788e503a6afc694e0eeb2ac0f6789d8
AlionaVr <alonavershilo@yandex.by>
2024-10-24 18:43:08 +0300

    MergeSorting

--- src/main/java/org/tasks/Main.java
+++ src/main/java/org/tasks/Main.java
@@ -1,14 +1,16 @@
 package org.tasks;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Library.askTheLibrarian();
         // Tax.calculatorOfTax();
         // PatternInText.findPattern();
         //Weather.calculatorOfAverageWeather();
         //CrossOrZero.play();
         // BinarySearch.start();
-
-
+        // Recursion.start();
+        // MergeSorting.start();
     }
 }
