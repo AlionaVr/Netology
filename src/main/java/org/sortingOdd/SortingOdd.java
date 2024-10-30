1c333bf37af4dfcfa2435108298f37e25b60c4ca
AlionaVr <alonavershilo@yandex.by>
2024-10-30 23:18:15 +0300

    sorting of array without Comarator

--- src/main/java/org/sortingOdd/SortingOdd.java
+++ src/main/java/org/sortingOdd/SortingOdd.java
@@ -1,33 +1,17 @@
 package org.sortingOdd;
 
-import java.util.Comparator;
-
-public class SortingOdd implements Comparator<Integer> {
+public class SortingOdd {
 
     public int[] sortArray(int[] array) {
         for (int i = 0; i <= array.length - 1; i++) {
-            if (isOdd(array[i])) {
-                for (int j = i + 1; j < array.length; j++) {
-                    if (isOdd(array[j])) {
-                        if (compare(array[i], array[j]) > 0) {
-                            int shift = array[i];
-                            array[i] = array[j];
-                            array[j] = shift;
-                        }
-                    }
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j] && array[i] % 2 == 1 && array[j] % 2 == 1) {
+                    int shift = array[i];
+                    array[i] = array[j];
+                    array[j] = shift;
                 }
             }
         }
         return array;
     }
-
-    @Override
-    public int compare(Integer num1, Integer num2) {
-        return num1.compareTo(num2);
-    }
-
-    public boolean isOdd(int num) {
-        return num % 2 != 0;
-    }
 }
-
