934cb2667d428c4445e62b592888190bec4ae9e2
AlionaVr <alonavershilo@yandex.by>
2024-11-06 15:44:50 +0300

    bubbleSort

--- src/main/java/org/tasks/Main.java
+++ src/main/java/org/tasks/Main.java
@@ -1,6 +1,7 @@
 package org.tasks;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,6 +14,9 @@ public class Main {
         // Recursion.start();
         // MergeSorting.start();
         // System.out.println(new Solution().solution(20));
-        System.out.println(Sum.recursiveSum(5));
+        //System.out.println(Sum.recursiveSum(5));
+
+        int[] arr = {1, 8, 3, 2, 7, 1};
+        System.out.println(Arrays.toString(BubbleSort.sort(arr)));
     }
 }
