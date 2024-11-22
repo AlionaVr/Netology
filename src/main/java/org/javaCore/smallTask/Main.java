fa8dac126024791f3cb896286ad30fa1e5477d64
AlionaVr <alonavershilo@yandex.by>
2024-11-22 20:23:46 +0300

    find numbers in array less than 90, add 10 to them and print array with this numbers

--- src/main/java/org/javaCore/smallTask/Main.java
+++ src/main/java/org/javaCore/smallTask/Main.java
@@ -1,12 +1,17 @@
 package org.javaCore.smallTask;
 
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        long count = IntStream.of(-5, -2, -10, -5, 1, 2, 3, 4, 5)
-                .filter(num -> num > 0)
-                .count();
-        System.out.println(count);
+        int[] array = {50, 60, 70, 80, 90, 100};
+        List<Integer> result = Arrays.stream(array)
+                .filter(num -> num < 90)
+                .map(num -> num + 10)
+                .limit(3).boxed()
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 }
