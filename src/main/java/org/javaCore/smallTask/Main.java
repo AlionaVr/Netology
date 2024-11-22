eea1de62297ccc4c945f969472004f0dcb54d366
AlionaVr <alonavershilo@yandex.by>
2024-11-22 20:48:37 +0300

    find word with >= 3 letters and print it in UpperCase

--- src/main/java/org/javaCore/smallTask/Main.java
+++ src/main/java/org/javaCore/smallTask/Main.java
@@ -2,16 +2,14 @@ package org.javaCore.smallTask;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {50, 60, 70, 80, 90, 100};
-        List<Integer> result = Arrays.stream(array)
-                .filter(num -> num < 90)
-                .map(num -> num + 10)
-                .limit(3).boxed()
-                .collect(Collectors.toList());
-        System.out.println(result);
+        String s = ("My Pen is black");
+        List<String> list = Arrays.asList(s.split(" "));
+        list.stream()
+                .filter(word -> word.length() >= 3)
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
     }
 }
