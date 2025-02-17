bc7fe605127ab1458a35ad2e29844ea36702a3a7
AlionaVr <alonavershilo@yandex.by>
2025-02-17 14:40:53 +0300

    add multithreading. executing for 8039 ms

--- src/main/java/org/javaCore/multithreadingSearchInterval/Main.java
+++ src/main/java/org/javaCore/multithreadingSearchInterval/Main.java
@@ -1,37 +1,29 @@
 package org.javaCore.multithreadingSearchInterval;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         String[] texts = new String[25];
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
         long startTs = System.currentTimeMillis(); // start time
+
         for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
-                        }
-                    }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
-                }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            Thread thread = new Thread(() -> findMaxSize(text));
+            threads.add(thread);
+            thread.start();
         }
+        for (Thread thread : threads) {
+            thread.join(); //ждём когда поток объект которого лежит в thread завершится, чтобы основной поток не закрылся раньше
+        }
+
         long endTs = System.currentTimeMillis(); // end time
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
@@ -44,5 +36,27 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    private static void findMaxSize(String text) {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
     }
 }
